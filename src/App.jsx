f401f40f66386187d3e893ed7733d2ddea77b7de
Ayush <ayushsindhwani08@gmail.com>
2022-04-27 20:12:52 +0530

    removed basename from router

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Navbar from "components/navbar";
 
 function App() {
   return (
-    <Router basename="/instagram">
+    <Router>
       <Navbar />
       <div className="container">
         <Routes />
